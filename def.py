7f3638985a614cea51e62e54471e20a7c861a432
huyremy <huynq@isi.com.vn>
2021-08-08 04:50:29 +0700

    Create def.py

---
+++ def.py
@@ -0,0 +1,21 @@
+import os
+import wave
+import pylab
+import warnings
+warnings.simplefilter("ignore", DeprecationWarning)
+def graph_spectrogram(wav_file):
+    sound_info, frame_rate = get_wav_info(wav_file)
+    pylab.figure(num=None, figsize=(10, 5))
+    pylab.subplot(111)
+    pylab.title('spectrogram of %r' % wav_file)
+    pylab.specgram(sound_info, Fs=frame_rate)
+    pylab.savefig('spectrogram.png')
+def get_wav_info(wav_file):
+    wav = wave.open(wav_file, 'r')
+    frames = wav.readframes(-1)
+    sound_info = pylab.fromstring(frames, 'int16')
+    frame_rate = wav.getframerate()
+    wav.close()
+    return sound_info, frame_rate
+graph_spectrogram('cough.wav')
+get_wav_info('cough.wav')
