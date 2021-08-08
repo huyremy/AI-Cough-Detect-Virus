6eb5ce4d2b6753d7fa2dbc658fbee963c888e355
huyremy <huynq@isi.com.vn>
2021-08-08 11:40:57 +0700

    Create feature.py

---
+++ feature.py
@@ -0,0 +1,27 @@
+import librosa
+import numpy as np
+import csv
+# Run the sound file and Clear data.
+audio = "cough.wav"
+y, sr = librosa.load(audio, mono=True, duration=1)
+# features
+chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
+spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
+spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
+rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
+zcr = librosa.feature.zero_crossing_rate(y)
+mfcc = librosa.feature.mfcc(y=y, sr=sr)
+# Clear data and Export to csv
+for feature in (chroma_stft, spec_cent, spec_bw, rolloff, zcr, mfcc):
+      with open ('feature.csv', 'a') as csvfile:
+            abc = np.mean(feature)
+            print(abc)
+            fieldnames = ['chroma_stft', 'spec_cent', 'spec_bw', 'rolloff', 'zcr', 'mfcc']
+            writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
+            writer.writerow({'chroma_stft':np.mean(chroma_stft),
+                             'spec_cent': np.mean(spec_cent),
+                             'spec_bw': np.mean(spec_bw),
+                             'rolloff': np.mean(rolloff),
+                             'zcr': np.mean(zcr),
+                             'mfcc': np.mean(mfcc)})
+            break
