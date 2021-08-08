129ed792565437251ff684f5794070edd268c74d
huyremy <huynq@isi.com.vn>
2021-08-08 12:20:06 +0700

    Update feature.py

--- feature.py
+++ feature.py
@@ -5,6 +5,7 @@ import csv
 audio = "cough.wav"
 y, sr = librosa.load(audio, mono=True, duration=1)
 # features
+rmse = librosa.feature.rms(y=x)
 chroma_stft = librosa.feature.chroma_stft(y=y, sr=sr)
 spec_cent = librosa.feature.spectral_centroid(y=y, sr=sr)
 spec_bw = librosa.feature.spectral_bandwidth(y=y, sr=sr)
@@ -12,13 +13,14 @@ rolloff = librosa.feature.spectral_rolloff(y=y, sr=sr)
 zcr = librosa.feature.zero_crossing_rate(y)
 mfcc = librosa.feature.mfcc(y=y, sr=sr)
 # Clear data and Export to csv
-for feature in (chroma_stft, spec_cent, spec_bw, rolloff, zcr, mfcc):
+for feature in (rmse, chroma_stft, spec_cent, spec_bw, rolloff, zcr, mfcc):
       with open ('feature.csv', 'a') as csvfile:
             abc = np.mean(feature)
             print(abc)
-            fieldnames = ['chroma_stft', 'spec_cent', 'spec_bw', 'rolloff', 'zcr', 'mfcc']
+            fieldnames = ['rmse', 'chroma_stft', 'spec_cent', 'spec_bw', 'rolloff', 'zcr', 'mfcc']
             writer = csv.DictWriter(csvfile, fieldnames=fieldnames)
-            writer.writerow({'chroma_stft':np.mean(chroma_stft),
+            writer.writerow({'rmse':np.mean(rmse),
+                             'chroma_stft':np.mean(chroma_stft),
                              'spec_cent': np.mean(spec_cent),
                              'spec_bw': np.mean(spec_bw),
                              'rolloff': np.mean(rolloff),
