5ecaf9a3219b436d3da7c70f0829df8cbdf71a76
huyremy <huynq@isi.com.vn>
2021-08-22 02:58:06 +0700

    Create detect.py

---
+++ detect.py
@@ -0,0 +1,35 @@
+import os
+import wave
+import pylab
+import warnings
+import matplotlib.pyplot as plt
+import librosa
+import numpy as np
+import csv
+import pathlib
+import argparse
+from define import *
+from check import*
+warnings.simplefilter("ignore", DeprecationWarning)
+warnings.filterwarnings('ignore')
+
+def get_args():
+    parser = argparse.ArgumentParser(
+        prog="Phát hiện virus qua tiếng ho",
+        description="Detect Virus via Cough Sound"
+    )
+    parser.add_argument(
+        '-w', '--wav', type=str, default='cough.wav',
+        help='Đường dẫn đến file wav chứa tiếng ho.'
+    )
+    args = parser.parse_args()
+
+    return args
+cough = load_model('cough.hdf5')
+if __name__ == '__main__':
+    args = get_args()
+    graph_spectrogram(args.wav)
+print("Convert done. AI đang kiểm tra. Vui lòng đợi trong giây lát.")
+output(cough,'huyremy.png',(224,224))
+os.remove("huyremy.png") 
+    
