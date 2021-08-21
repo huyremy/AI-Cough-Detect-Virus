6f4e61f05c17b5b5dba9da70d85eb95ae3ba083a
huyremy <huynq@isi.com.vn>
2021-08-22 03:10:19 +0700

    Create check.py

---
+++ check.py
@@ -0,0 +1,22 @@
+import tensorflow 
+from tensorflow.keras.layers import *
+from tensorflow.keras.preprocessing import image  
+from tensorflow.keras.models import *
+import numpy as np
+import matplotlib.pyplot as plt
+import matplotlib.image as mpimg
+import warnings
+warnings.simplefilter("ignore", DeprecationWarning)
+warnings.filterwarnings('ignore')
+
+def output(model,img_path,size):
+    img_path=img_path
+    img = image.load_img(img_path,target_size=size)
+    img_arr = image.img_to_array(img,dtype='double')
+    img_arr=img_arr/255
+    img_arr=np.expand_dims(img_arr,axis=0)
+    result = np.argmax(model.predict(img_arr), axis=-1)
+    if result==1:
+        print("Dương tính")
+    elif result==0:
+        print("Âm tính")
