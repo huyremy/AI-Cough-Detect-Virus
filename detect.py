9cfa2d8469ecdc586336677e8d030d8c779cf2d8
huyremy <huynq@isi.com.vn>
2021-08-22 03:06:05 +0700

    Update detect.py

--- detect.py
+++ detect.py
@@ -10,20 +10,10 @@ import pathlib
 import argparse
 from define import *
 from check import*
-warnings.simplefilter("ignore", DeprecationWarning)
-warnings.filterwarnings('ignore')
-
 def get_args():
-    parser = argparse.ArgumentParser(
-        prog="Phát hiện virus qua tiếng ho",
-        description="Detect Virus via Cough Sound"
-    )
-    parser.add_argument(
-        '-w', '--wav', type=str, default='cough.wav',
-        help='Đường dẫn đến file wav chứa tiếng ho.'
-    )
+    parser = argparse.ArgumentParser(prog="Phát hiện virus qua tiếng ho",description="Detect Virus via Cough Sound")
+    parser.add_argument('-w', '--wav', type=str, default='cough.wav',help='Đường dẫn đến file wav chứa tiếng ho.')
     args = parser.parse_args()
-
     return args
 cough = load_model('cough.hdf5')
 if __name__ == '__main__':
@@ -32,4 +22,3 @@ if __name__ == '__main__':
 print("Convert done. AI đang kiểm tra. Vui lòng đợi trong giây lát.")
 output(cough,'huyremy.png',(224,224))
 os.remove("huyremy.png") 
-    
